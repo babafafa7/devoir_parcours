e2a0a932b400f29df8931833ceaff90cf569f66c
babafafa7 <bastien.falchero@outlook.fr>
2020-12-04 13:42:18 +0100

    Debut implémentation de la structur parcours en profondeur

--- parcours_profondeur.c
+++ parcours_profondeur.c
@@ -1,23 +1,49 @@
 #include "parcours_profondeur.h"
-#include "graph_mat-1.c"
-#include "liste.c"
 
-parcours_largeur* parcours_largeur_construire(int taille_listes, int sommet_depart){
+parcours_profondeur* parcours_profondeur_construire(int taille_listes, int sommet_depart){
+    parcours_profondeur* parcours;
 
+    /* __ vérification des paramètres */
+	if((taille_listes <= 0) || (sommet_depart <= 0)){
+		return NULL;
+    }
+
+    /* __ allocation mémoire pour le parcours */
+    parcours = (parcours_profondeur*) malloc(sizeof(parcours_profondeur));
+
+    if(parcours != NULL){
+        /* __sommet de départ */
+        parcours->d = sommet_depart;
+
+        /* __Allocations des listes*/
+        parcours->prefixe = liste_construire(taille_listes);
+        parcours->suffixe = liste_construire(taille_listes);
+        parcours->pere = liste_construire(taille_listes);
+    }
+     return parcours;
 }
 
-void detruire_parcours_profondeur(parcours_largeur*){
-
+void detruire_parcours_profondeur(parcours_profondeur* p){
+    if(p != NULL){
+       /* __ libération de la mémoire allouée pour les listes de la structure */
+       if(p->prefixe != NULL){
+           liste_detruire(p->prefixe);
+           liste_detruire(p->suffixe);
+           liste_detruire(p->pere);
+       }
+    }
+    /* __ libération de la mémoire allouée pour la structure elle-même */
+	free(p);
 }
 
-parcours_largeur* parcours_en_largeur_iter(graph_mat*, int sommet_depart){
+parcours_profondeur* parcours_en_profondeur_iter(graph_mat*, int sommet_depart){
     
 }
 
-parcours_largeur* parcours_en_largeur_iter(graph_mat*, int sommet_depart){
+parcours_profondeur* parcours_en_profondeur_iter(graph_mat*, int sommet_depart){
     
 }
 
-void afficher_listes(parcours_largeur*){
-    
+void afficher_listes(parcours_profondeur*){
+
 }
--- parcours_profondeur.h
+++ parcours_profondeur.h
@@ -1,22 +1,24 @@
 #include "liste.h"
 #include "graph_mat-1.h"
+#include <stdlib.h>
 
-#ifndef PARCOURS_LARGEUR_H
-#define PARCOURS_LARGEUR_H
 
-struct s_parcours_largeur {
-    int d;              //sommet départ du parcours
-    liste* prefixe;     //liste d'ordre de visite des sommets
-    liste* suffixe;     //liste d'ordre de sommets totalement exploré
-    liste* pere;        //liste des pères de chaque sommet dans l'arborescence du parcours 
+#ifndef PARCOURS_PROFONDEUR_H
+#define PARCOURS_PROFONDEUR_H
+
+struct s_parcours_profondeur {
+    int d;              /* __sommet départ du parcours */
+    liste** prefixe;    /* __liste d'ordre de visite des sommets */
+    liste** suffixe;    /* __liste d'ordre de sommets totalement exploré */
+    liste** pere;       /* __liste des pères de chaque sommet dans l'arborescence du parcours */ 
 };
 
-typedef struct s_parcours_largeur parcours_largeur;
+typedef struct s_parcours_profondeur parcours_profondeur;
 
-parcours_largeur* parcours_largeur_construire(int taille_listes, int sommet_depart);
-void detruire_parcours_profondeur(parcours_largeur*);
-parcours_largeur* parcours_en_largeur_iter(graph_mat*, int sommet_depart);
-parcours_largeur* parcours_en_largeur_rec(graph_mat*, int sommet_depart);
-void afficher_listes(parcours_largeur*);
+parcours_profondeur* parcours_profondeur_construire(int taille_listes, int sommet_depart);
+void detruire_parcours_profondeur(parcours_profondeur*);
+parcours_profondeur* parcours_en_profondeur_iter(graph_mat*, int sommet_depart);
+parcours_profondeur* parcours_en_profondeur_rec(graph_mat*, int sommet_depart);
+void afficher_listes(parcours_profondeur*);
 
 #endif
