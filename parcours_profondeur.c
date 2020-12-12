8277156888480202c8b9ddc8a33370a3fa7dceb8
babafafa7 <bastien.falchero@outlook.fr>
2020-12-12 18:04:47 +0100

    modif commentaires

--- parcours_profondeur.c
+++ parcours_profondeur.c
@@ -33,7 +33,7 @@ parcours_profondeur* parcours_profondeur_construire(int taille_listes, int somme
 
 void detruire_parcours_profondeur(parcours_profondeur* p){
     if(p != NULL){
-        /* __ libération de la mémoire allouée pour les listes de la structure */
+        /* __ libération de la mémoire allouée pour les listes et le tableau de la structure */
         liste_detruire(&(p->prefixe));
         liste_detruire(&(p->pile));
         liste_detruire(&(p->suffixe));
@@ -45,29 +45,35 @@ void detruire_parcours_profondeur(parcours_profondeur* p){
 }
 
 parcours_profondeur* parcours_en_profondeur_iter(graph_mat * g , int sommet_depart){
+    unsigned int i, j;
+    int *elt, brk;
     parcours_profondeur* p = parcours_profondeur_construire(gm_n(g), sommet_depart);
-    unsigned int i;
+    
+    if(p == NULL){
+        return p;                                           /* __Si le parcours a eut un problème lors de l'initialisation ont retourne un parcours null */
+    }
+    
     while(!liste_est_pleine(p->prefixe)){                   /* __Tant que l'on a pas visité tous les sommets */
         if (liste_est_vide(p->prefixe)){                    /* __ Si le parcours est vide */
             liste_ajouter_debut(p->pile, sommet_depart);    /* __On place le sommet de depart dans la pile */
-            liste_ajouter_fin(p->prefixe, sommet_depart);
+            liste_ajouter_fin(p->prefixe, sommet_depart);   /* __On le marque comme visité (on le place dans la liste prefixe) */
         }
-        else{                                               /* __Sinon on cherche un sommet non visité différent du sommet de départ */
+        else{                                               /* __Sinon on cherche un sommet non visité (qui n'est pas dans la liste prefixe) */
             for (i = 0; i < gm_n(g); i++){
                 if(!liste_contient_element(p->prefixe, i)){
                     liste_ajouter_debut(p->pile, i);        /* __On le place dans la pile */
                     liste_ajouter_fin(p->prefixe, i);       /* __On le marque comme visité (on le place dans la liste prefixe) */
-                    p->pere[i]=-1;                          /* __Puis on marque le père du sommet (-1) car nouvelle composante connexe */
+                    p->pere[i] = -1;                        /* __Puis on marque le père (-1) car nouvelle composante connexe */
                     break;
                 }
             } 
         }
         while(!liste_est_vide(p->pile)){                    /* __Tant que la pile n'est pas vide */
-            int* elt = liste_get_debut(p->pile);            /* __On prend element au sommet de la pile */
-            int brk = 1;
-            unsigned int j;
+            elt = liste_get_debut(p->pile);                 /* __On prend un element au sommet de la pile */
+            brk = 1;
+            
             for(j = 0; j < gm_n(g); j++){                   /* __On cherche s'il a des fils non visités */
-                if(((gm_mult_edge(g,(*elt),j)) >= 1) && (liste_contient_element(p->prefixe,j) != 1)){ 
+                if(((gm_mult_edge(g,(*elt),j)) >= 1) && (!liste_contient_element(p->prefixe,j))){ 
                     brk = 0;
                     liste_ajouter_debut(p->pile, j);        /* __Si oui on place le fils en tete de pile */ 
                     liste_ajouter_fin(p->prefixe, j);       /* __et on le marque comme visité (on le place dans la liste prefixe) */
@@ -85,17 +91,19 @@ parcours_profondeur* parcours_en_profondeur_iter(graph_mat * g , int sommet_depa
 }
 
 parcours_profondeur* parcours_en_profondeur_rec(graph_mat* g, parcours_profondeur* p, int sommet_depart){
-    int brk, *elt;
-    unsigned int i,j;
+    int  *elt, brk;
+    unsigned int i, j;
+
     if(p == NULL){
         p = parcours_profondeur_construire(gm_n(g), sommet_depart);
     }
+    
     if(sommet_depart != -1){                                        /* __Si le sommet de départ n'est pas le sommet -1 */
         liste_ajouter_fin(p->prefixe, sommet_depart);               /* __On le marque comme visité (on le place dans la liste prefixe */
         elt = &(sommet_depart);
         brk = 1;
         for(j = 0; j < gm_n(g); j++){                               /* __On cherche s'il a des fils non visités */
-            if(((gm_mult_edge(g,*elt,j)) >= 1) && (liste_contient_element(p->prefixe,j) != 1)){
+            if(((gm_mult_edge(g,*elt,j)) >= 1) && (!liste_contient_element(p->prefixe,j))){
                 brk = 0;                            
                 p->pere[j] = sommet_depart;                         /* __Si oui on marque le pere du fils */
                 sommet_depart = j;                                  /* __puis on marque le fils comme sommet de depart */
